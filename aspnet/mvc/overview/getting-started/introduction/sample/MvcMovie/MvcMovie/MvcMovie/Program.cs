f64965dd95b13caddd798b32e683b5ac3b2511a3
zas11053 <zas11053@uga.edu>
2022-03-11 01:02:12 -0500

    Steps 5 - 7
    Steps 5 - 7 for Assignment 8. I am having trouble with Step 8 so I am committing this until I can figure that out.

--- aspnet/mvc/overview/getting-started/introduction/sample/MvcMovie/MvcMovie/MvcMovie/Program.cs
+++ aspnet/mvc/overview/getting-started/introduction/sample/MvcMovie/MvcMovie/MvcMovie/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using MvcMovie.Data;
+//using MvcMovie.Data;
 using MvcMovie.Models;
 
 var builder = WebApplication.CreateBuilder(args);
